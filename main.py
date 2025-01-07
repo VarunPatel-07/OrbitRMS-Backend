ef9da638f14862b1852ae4ce902c3720ba46c86d
varun <VarunSureshkumarPatel07@gmail.com>
2025-01-07 22:42:37 +0530

    started working on the backend with python and now working on the authentication

--- main.py
+++ main.py
@@ -1,15 +1,5 @@
-from typing import Union
-
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from routes.auth.login import login
 
 app = FastAPI()
-
-
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
-
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: str| None = None):
-    return {"item_id": item_id, "q": q}
+app.include_router(login)
---
+++ routes/auth/login.py
@@ -0,0 +1,4 @@
+from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordRequestForm
+
+login = APIRouter()
