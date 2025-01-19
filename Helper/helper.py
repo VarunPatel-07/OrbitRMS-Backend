7261b081a7c653d0416f73cd8faf8604c9bbfbff
varun <VarunSureshkumarPatel07@gmail.com>
2025-01-19 23:55:22 +0530

    now started working on the api to create organization and also to verify the related info

--- Helper/helper.py
+++ Helper/helper.py
@@ -1,2 +1,10 @@
+import secrets
+
+
 def generate_full_name(first_name:str , last_name:str = None) -> str:
  return f"{first_name} {last_name}"
+
+def generate_random_secret_key()->str:
+    generated_secret_key = secrets.token_urlsafe(16)
+    print(generated_secret_key)
+    return generated_secret_key
--- routes/auth/authentication.py
+++ routes/auth/authentication.py
@@ -7,14 +7,14 @@ from PydenticModels.UserModels import LoginUserInfo  ,SignUpUserInfo
 from Middelware.verifyToken import oauth2_scheme , verify_token
 
 
-routes = APIRouter(
+authRoutes = APIRouter(
     prefix="/app/v1/auth",
     tags=["auth"]
 )
 
 
 # This is the api that is used to signUp user
-@routes.post(path="/signUp", status_code=status.HTTP_201_CREATED)
+@authRoutes.post(path="/signUp", status_code=status.HTTP_201_CREATED)
 async def sign_up(
     db:db_dependencies , user_info: SignUpUserInfo = Depends(SignUpUserInfo.as_form)):
     try:
@@ -76,7 +76,7 @@ async def sign_up(
 
 
 # This is The Api that is used to log in the use
-@routes.post(path="/login" , status_code=status.HTTP_201_CREATED)
+@authRoutes.post(path="/login" , status_code=status.HTTP_201_CREATED)
 async def login(
     db:db_dependencies, user_info: LoginUserInfo = Depends(LoginUserInfo.as_form)):
     try:
@@ -118,7 +118,7 @@ async def login(
 
 
 # this is the api to verify the users token
-@routes.get(path="/verify-user" , status_code=status.HTTP_200_OK)
+@authRoutes.get(path="/verify-user" , status_code=status.HTTP_200_OK)
 async def verify_user(db:db_dependencies , token:str = Depends(oauth2_scheme)):
     token_payload = verify_token(token)
     if not token_payload:
@@ -126,8 +126,17 @@ async def verify_user(db:db_dependencies , token:str = Depends(oauth2_scheme)):
     find_user = db.query(Models.User).filter(Models.User.id == token_payload).first()
     if not find_user:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
-    return {"message":"The User Is Verified Successfully", "success":True}
-
-
-
-# this is the api to get the user info
+    
+    user = {
+        "id": find_user.id,
+        "username": find_user.username,
+        "email": find_user.email,
+        "first_name": find_user.first_name,
+        "last_name": find_user.last_name,
+        "full_name": find_user.full_name,
+        "default_organization_id": find_user.default_organization_id,
+        "organizations": find_user.Organizations,
+        "profile_picture": find_user.profile_picture,
+        }
+    
+    return {"message":"The User Is Verified Successfully", "success":True , "user_info":user}
