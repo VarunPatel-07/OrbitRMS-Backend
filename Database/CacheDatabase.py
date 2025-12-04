84b4ce36a71bcafe6d5673153230d187b26de532
varun <VarunSureshkumarPatel07@gmail.com>
2025-12-04 20:07:26 +0530

    chore: currently Removimng the Google Recaptcha

--- Database/CacheDatabase.py
+++ Database/CacheDatabase.py
@@ -1,5 +1,5 @@
 from dotenv import load_dotenv
-from valkey import Valkey
+from redis.asyncio import Redis
 
 from Config.EnvConfig import EnvConfig
 
@@ -16,15 +16,14 @@ CACHED_DATABASE_PASSWORD = EnvConfig.CACHED_DATABASE_PASSWORD
 
 
 if BACKEND_APP_ENVIRONMENT == "PRODUCTION":
-    cache_database = Valkey(
+    cache_database = Redis(
         host=CACHED_DATABASE_HOST,
         port=CACHED_DATABASE_PORT,
         password=CACHED_DATABASE_PASSWORD,
         ssl=True,
-        ssl_cert_reqs=None,  # Aiven uses self-signed certs
         decode_responses=True,
     )
 else:
-    cache_database = Valkey(
+    cache_database = Redis(
         host=CACHED_DATABASE_HOST, port=CACHED_DATABASE_PORT, decode_responses=True
     )
