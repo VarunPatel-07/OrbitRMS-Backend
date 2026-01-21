f9aa229822dd6aae7b7ec8090c8685815896ae75
varun <VarunSureshkumarPatel07@gmail.com>
2026-01-21 12:47:26 +0530

    chore: done the change for the production

--- manage_logs.sh
+++ manage_logs.sh
@@ -4,22 +4,28 @@ source .env
 # Lets first Define the Path  for the logs and then we will define two logs path like archive and then we will define the logs for the Archive logs
 LOGS_DIR=$PRODUCTION_LOGS_STORE_DIR
 ARCHIVE_LOGS_DIR="$LOGS_DIR/archive"
+RUNTIME_LOGS="$LOGS_DIR/runtime"
+FAILURES_LOGS="$LOGS_DIR/failures"
+ARCHIVE_RUNTIME_LOGS="$ARCHIVE_LOGS_DIR/runtime"
+ARCHIVE_FAILURES_LOGS="$ARCHIVE_LOGS_DIR/failures"
+LOG_MANAGEMENT_FILE_PATH="$LOGS_DIR/log_management.log"
+
 
 # Lets first define the two folder that exist and if not then we will make this function
-mkdir -p "$ARCHIVE_LOGS_DIR/runtime"
-mkdir -p "$ARCHIVE_LOGS_DIR/failures"
+mkdir -p "$ARCHIVE_RUNTIME_LOGS"
+mkdir -p "$ARCHIVE_FAILURES_LOGS"
 
 # Now we will move all the compressed run time logs into the runtime folder in the archive
-find "$LOGS_DIR/runtime" -name "*.gz" -type f -mtime +7 -exec mv {} "$ARCHIVE_LOGS_DIR/runtime/" \;
+find "$RUNTIME_LOGS" -name "*.gz" -type f -mtime +7 -exec mv {} "$ARCHIVE_RUNTIME_LOGS/" \;
 
 #  Now We will move all the compressed run time logs into the failures folder in the archive
-find "$LOGS_DIR/failures" -name "*.gz" -type f -mtime +7 -exec mv {} "$ARCHIVE_LOGS_DIR/failures/" \;
+find "$FAILURES_LOGS" -name "*.gz" -type f -mtime +7 -exec mv {} "$ARCHIVE_FAILURES_LOGS/" \;
 
 # Now We will Delete the logs in the archive/runtime folder that are older then the 30 days then we will delete this 
-find "$ARCHIVE_LOGS_DIR/runtime" -name "*.gz" -type f -mtime +30 -exec rm {} \;
+find "$ARCHIVE_RUNTIME_LOGS" -name "*.gz" -type f -mtime +30 -exec rm {} \;
 
 # Now We will Delete the logs in the archive/failures folder that are older then the 30 days then we wll delete this 
-find "$ARCHIVE_LOGS_DIR/failures" -name "*.gz" -type f -mtime +30 -exec rm {} \;
+find "$ARCHIVE_FAILURES_LOGS" -name "*.gz" -type f -mtime +30 -exec rm {} \;
 
 # We will write the logs for this also 
-echo "$(date): Log management completed." >> "$LOGS_DIR/log_management.log"
+echo "$(date): Log management completed." >> "$LOG_MANAGEMENT_FILE_PATH"
--- monitoring.py
+++ monitoring.py
@@ -1,15 +1,20 @@
 import sentry_sdk
 from config.EnvConfig import EnvConfig
+from constants.constant import (
+    PRODUCTION_ENVIRONMENT,
+    SENTRY_TRACE_SAMPLE_RATE,
+    SENTRY_SEND_DEFAULT_PII,
+)
 from sentry_sdk.integrations.fastapi import FastApiIntegration
 
 
 def initSentryMonitoring():
 
-    if EnvConfig.BACKEND_APP_ENVIRONMENT == "PRODUCTION":
+    if EnvConfig.BACKEND_APP_ENVIRONMENT == PRODUCTION_ENVIRONMENT:
         sentry_sdk.init(
             dsn=EnvConfig.SENTRY_MONITORING_KEY,
             integrations=[FastApiIntegration()],
             environment=EnvConfig.BACKEND_APP_ENVIRONMENT,
-            traces_sample_rate=1.0,
-            send_default_pii=False,
+            traces_sample_rate=SENTRY_TRACE_SAMPLE_RATE,
+            send_default_pii=SENTRY_SEND_DEFAULT_PII,
         )
